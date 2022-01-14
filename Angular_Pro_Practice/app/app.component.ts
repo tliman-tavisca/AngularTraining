acc245397c9847e39586efd7841762fb628686ce
tliman-tavisca <71818283+tliman-tavisca@users.noreply.github.com>
2022-01-14 22:47:44 +0530

    Dynamic Component
    Dynamic Component

--- Angular_Pro_Practice/app/app.component.ts
+++ Angular_Pro_Practice/app/app.component.ts
@@ -1,34 +1,101 @@
-import { Component } from "@angular/core";
+import {
+  AfterContentInit,
+  Component,
+  ComponentFactoryResolver,
+  ComponentRef,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
+} from "@angular/core";
 import { User } from "./auth-form/auth-form.interface";
+
+import { AuthFormDynamicComponent } from "./auth-form/auth-form-dynamic.component";
 
 @Component({
   selector: "app-root",
   styleUrls: ["app.component.scss"],
   template: `
-    <div class="app">
-      <auth-form (submitted)="createUser($event)">
-        <h3>Create Account</h3>
-        <button type="submit">Join us</button>
-      </auth-form>
-      <auth-form (submitted)="loginUser($event)">
-        <h3>Login</h3>
-        <auth-remember (checked)="remeberUser($event)"></auth-remember>
-        <button type="submit">Login</button>
-      </auth-form>
+    <div>
+      <div>
+        <button (click)="destroyComponent()">Destory</button>
+      </div>
+      <div>
+        <button (click)="moveComponent()">Move</button>
+      </div>
+
+      <div>
+        Dynamic Component
+        <div #entry></div>
+      </div>
+
+      <!-- Declarartive way to add dynamic temlpate  -->
+      <ng-container [ngTemplateOutlet]="tmpl" context="ctx"> </ng-container>
+
+      <div>
+        <h3>Dynamic template</h3>
+        <template #tmpl let-name let-location="location">
+          {{ name }} : {{ location }}
+        </template>
+      </div>
     </div>
   `,
 })
-export class AppComponent {
+export class AppComponent implements AfterContentInit {
+  component: ComponentRef<AuthFormDynamicComponent>;
+  @ViewChild("entry", { read: ViewContainerRef }) entry: ViewContainerRef;
+
+  @ViewChild("tmpl") tmpl: TemplateRef<any>;
+
+  constructor(private resolver: ComponentFactoryResolver) {}
+
+  ngAfterContentInit(): void {
+    const authFormFactory = this.resolver.resolveComponentFactory(
+      AuthFormDynamicComponent
+    );
+
+    this.entry.createComponent(authFormFactory);
+
+    // to set order of dynamic compenent pass order
+    this.component = this.entry.createComponent(authFormFactory, 0);
+
+    // Instead of passing input to component dynamically update the property
+    this.component.instance.title = "Create Account";
+
+    // Dynamically subsribe for events of event emitter i.e. @output
+    this.component.instance.submitted.subscribe(this.loginUser);
+
+    // Dynamic template
+    this.entry.createEmbeddedView(this.tmpl, {
+      // Use $implicit will bind first value for name without property name
+      $implicit: "Test",
+      location: "Pune, IN",
+    });
+  }
+
   rememberMe: boolean = false;
   createUser(user: User) {
     console.log("Create Account", user);
   }
 
   loginUser(user: User) {
-    console.log("Login", user, this.rememberMe);
+    console.log("Login", user);
   }
 
   remeberUser(remeber: boolean) {
     this.rememberMe = remeber;
   }
+
+  destroyComponent() {
+    this.component.destroy();
+  }
+
+  moveComponent() {
+    this.entry.move(this.component.hostView, 1);
+  }
+
+  // dynamic template object passing using declarative way
+  ctx = {
+    $implicit: "Ctx Test",
+    location: "Ctx Pune, IN",
+  };
 }
--- Angular_Pro_Practice/app/app.module.ts
+++ Angular_Pro_Practice/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AuthFormModule } from "./auth-form/auth-form.module";
 
 import { AppComponent } from "./app.component";
+import { AuthFormDynamicComponent } from "./auth-form/auth-form-dynamic.component";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AuthFormModule],
   bootstrap: [AppComponent],
+  entryComponents: [AuthFormDynamicComponent],
 })
 export class AppModule {}
