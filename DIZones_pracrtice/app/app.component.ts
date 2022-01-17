c80a3c581acfa839598808abc2d147365f2094a2
tliman-tavisca <71818283+tliman-tavisca@users.noreply.github.com>
2022-01-18 01:04:12 +0530

    Zone to track async state and events
    Zone to track async state and events

--- DIZones_pracrtice/app/app.component.ts
+++ DIZones_pracrtice/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, DoCheck, NgZone, OnInit } from "@angular/core";
 import { FoodStoreService } from "./food-store/food-store.service";
 
 @Component({
@@ -15,6 +15,7 @@ import { FoodStoreService } from "./food-store/food-store.service";
     `,
   ],
   template: `
+    <!--
     <div>
       <pizza-viewer></pizza-viewer>
       <side-viewer></side-viewer>
@@ -22,9 +23,32 @@ import { FoodStoreService } from "./food-store/food-store.service";
     </div>
 
     <div>Food Store ({{ (store | async)?.name }})</div>
+    -->
+
+    <div>Counter; {{ counter }}</div>
   `,
 })
-export class AppComponent {
+export class AppComponent implements OnInit, DoCheck {
   store = this.foodService.getStore();
-  constructor(private foodService: FoodStoreService) {}
+  constructor(private foodService: FoodStoreService, private zone: NgZone) {}
+
+  counter = 0;
+
+  ngOnInit(): void {
+    this.zone.runOutsideAngular(() => {
+      for (let u = 0; u < 50; u++) {
+        setTimeout(() => {
+          this.counter++;
+        });
+      }
+      this.zone.run(() => {
+        setTimeout(() => {
+          this.counter = this.counter;
+        }, 1000);
+      });
+    });
+  }
+  ngDoCheck(): void {
+    console.log("Change detection ");
+  }
 }
