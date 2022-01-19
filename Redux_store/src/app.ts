574efc71d723347f83d92bbaa3d17ce2d5b03194
tliman-tavisca <71818283+tliman-tavisca@users.noreply.github.com>
2022-01-19 14:26:48 +0530

    Add reducer implementation
    Add reducer implementation

--- Redux_store/src/app.ts
+++ Redux_store/src/app.ts
@@ -7,14 +7,11 @@ const button = document.querySelector("button") as HTMLButtonElement;
 const destroy = document.querySelector(".unsubscribe") as HTMLButtonElement;
 const todoList = document.querySelector(".todos") as HTMLLIElement;
 
-const store = new fromStore.Store(
-  {},
-  {
-    todos: [{ label: "Eat Pizza", complete: false }],
-  }
-);
+const reducers = {
+  todos: fromStore.reducer,
+};
 
-console.log(store.value);
+const store = new fromStore.Store(reducers);
 
 button.addEventListener(
   "click",
@@ -23,7 +20,11 @@ button.addEventListener(
 
     const payload = { label: input.value, complete: false };
 
-    console.log(payload);
+    store.dispatch({
+      type: "ADD_TODO",
+      payload: payload,
+    });
+    console.log(store.value);
 
     input.value = "";
   },
--- Redux_store/src/store/store.ts
+++ Redux_store/src/store/store.ts
@@ -4,10 +4,24 @@ export class Store {
   private state: { [key: string]: any };
 
   constructor(reducers = {}, initialState = {}) {
-    this.state = initialState;
+    this.reducers = reducers;
+    this.state = this.reduce(initialState, {});
   }
 
   get value() {
     return this.state;
+  }
+
+  dispatch(action) {
+    this.state = this.reduce(this.state, action);
+  }
+
+  private reduce(state, action) {
+    const newState = {};
+    for (const prop in this.reducers) {
+      newState[prop] = this.reducers[prop](state[prop], action);
+    }
+
+    return newState;
   }
 }
