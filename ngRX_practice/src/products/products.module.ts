6f6d2dcfb2f6f55ced5907e49faa13716c0cd278
tliman-tavisca <71818283+tliman-tavisca@users.noreply.github.com>
2022-01-21 21:44:08 +0530

    Add Guards
    Add Guards

--- ngRX_practice/src/products/products.module.ts
+++ ngRX_practice/src/products/products.module.ts
@@ -17,19 +17,25 @@ import * as fromContainers from "./containers";
 // services
 import * as fromServices from "./services";
 
+// guards
+import * as fromGuards from "./guards";
+
 // routes
 export const ROUTES: Routes = [
   {
     path: "",
+    canActivate: [fromGuards.PizzasGuard],
     component: fromContainers.ProductsComponent,
   },
 
   {
     path: "new",
+    canActivate: [fromGuards.PizzasGuard, fromGuards.ToppingsGuard],
     component: fromContainers.ProductItemComponent,
   },
   {
     path: ":pizzaId",
+    canActivate: [fromGuards.PizzaExistsGuards, fromGuards.ToppingsGuard],
     component: fromContainers.ProductItemComponent,
   },
 ];
@@ -43,7 +49,7 @@ export const ROUTES: Routes = [
     StoreModule.forFeature("products", reducers),
     EffectsModule.forFeature(effects),
   ],
-  providers: [...fromServices.services],
+  providers: [...fromServices.services, ...fromGuards.guards],
   declarations: [...fromContainers.containers, ...fromComponents.components],
   exports: [...fromContainers.containers, ...fromComponents.components],
 })
