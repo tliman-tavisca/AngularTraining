6ab189fa8d7c7a938dc14125e284dca072c8f265
tliman-tavisca <71818283+tliman-tavisca@users.noreply.github.com>
2022-01-25 19:24:51 +0530

    LIt with events
    LIt with events

--- LIT_Practice/src/main.ts
+++ LIT_Practice/src/main.ts
@@ -2,3 +2,4 @@ import "./elements/simple-greetings";
 import "./elements/textbox-element";
 import "./elements/my-element";
 import "./elements/element-with-classpap";
+import "./elements/event_element";
